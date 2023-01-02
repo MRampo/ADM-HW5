17a0ff3fa6ac4e5e850b15cc0389baa2d06e2e33
Matteo Rampolla <matteo.rampolla96@gmail.com>
2023-01-02 17:01:46 +0100

    Added Visualization 1 and 3

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,8 +1,8 @@
 most_popular=$(awk -F'/t' 'BEGIN { SUBSEP = OFS = FS } { s[$1,$2] += 1} END { for (i in s) { print  s[i], i } }' hero-network.csv  | sort -k1 -n  | tail -n 1 | awk -F'/t' '{print $2}')
 
-n_comic_per_hero=$(awk -F',' 'BEGIN { SUBSEP = OFS = FS } { s[$1] += 1} END { for (i in s) { print i, s[i]}}' edges.csv )
+n_comic_per_hero=$(awk -F',' 'BEGIN {} { s[$1] += 1} END { for (i in s) { print  s[i],i}}' edges.csv | sort -k1 -n -r  | head -n 10 )
 
-average_hero_comic=$(awk -F',' 'BEGIN { SUBSEP = OFS = FS } { s[$2] += 1 c++} END {{ for (i in s) { x++ }} print c/x }' edges.csv ) 
+average_hero_comic=$(awk -F',' 'BEGIN {} { f[$2] += 1 c++} END {{ for (i in f) {print i x++ }} print x }' edges.csv ) 
 
 
 echo $most_popular

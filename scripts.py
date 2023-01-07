26f46e559ae6ef7405000d0fb98399bf167f8740
Maria Vittoria Vestini <113996044+MaviVestini@users.noreply.github.com>
2023-01-07 18:32:40 +0100

    Small changes in scripts

--- scripts.py
+++ scripts.py
@@ -4,6 +4,219 @@ import re
 import numpy as np
 from queue import Queue 
 
+
+
+# Common functions
+
+
+def findNHeroes(data: pd.DataFrame, N: int) -> pd.DataFrame:
+    NHeroes = list(data.groupby(by="hero")
+    .count()                                            #count number of repetition
+    .sort_values(by="comic", ascending=False)           #Sort values by "comic"
+    .head(N)                                            #get top N
+    .reset_index()                                      #reset index
+    .hero)                                              #get only the names of our heroes
+    return NHeroes
+
+
+
+
+# Functionality 1
+
+
+def findNetworksHub(graph, typeG, degreeDist):
+    if typeG == 1:
+
+        #return a list with just the degree of the graph
+        tempDegree = dict(graph.degree())
+
+    #if graph of type 2
+    if typeG == 2:
+        #create a dictionary of the nosed in G2
+        dic = dict(graph.nodes(data="type"))
+
+        #we take only the degree for the comics
+        tempDegree = dict(graph.degree([k for k, v in dic.items() if v == "comic"]))
+
+    #select the threshold
+    threshold = np.quantile(list(tempDegree.values()), 0.95)
+    
+    #Select nodes based on threshold
+    nodes = [k for k, v in tempDegree.items() if v > threshold ]
+
+    return nodes
+
+
+def findNNodes(graph, typeG, edges):
+    #take the number of nodes
+    numberNodes = len(graph)
+    
+    #if graph is of type 2
+    if typeG == "2":
+        
+        #create a dictionary from the nodes of the graph
+        dic = dict(graph.nodes(data="type"))
+
+        #select all the nodes that are comics
+        numberComic = len([k for k, v in dic.items() if v == "comic"])
+        
+        #select all the nodes that are heroes
+        numberHero = len([k for k, v in dic.items() if v == "hero"])
+
+        #put them in a tuple
+        numberNodes = (numberComic, numberHero)
+    return numberNodes
+
+
+def findNCollaboration(graph, typeG):
+    #If graph is of type 1
+    if typeG == 1:
+
+        #return a list with just the degree of the graph
+        return list(graph.degree())
+
+    #if graph is of type 2
+    if typeG == 2:
+        
+        #select all the nodes that are comic and take the degree
+        dic = dict(graph.nodes(data="type"))
+        return list(graph.degree([k for k, v in dic.items() if v == "comic"]))
+
+
+
+
+# Functionality 3
+
+
+def writePath(prev, source, target):
+    #create empty list
+    myList = []
+
+    #create boolean in case the path is not found
+    notFound = True
+    
+    #place the target in the list
+    myList += [target]
+
+    #if the value is a NaN
+    if prev[target] == np.nan:
+        return "Path Not Found!"
+    
+    #while True
+    while notFound:
+
+        #add the last node found in reverse
+        myList += [prev[myList[-1]]]
+
+        #if last one inserted is the source
+        if myList[-1] == source:
+
+            #then i found a path
+            notFound= False
+    
+    # reverse the list
+    myList.reverse()
+    return myList
+
+
+def findShortestPath(graph, couple):
+    #select the source and the target
+    source = couple[0]
+    target = couple[1]
+
+    #create a set of nodes to see which one we will have already visited
+    nodesNotVisited = set(graph.nodes())
+    
+    #Create dist dictionary with np.inf as values
+    dist = {node:np.inf for node in nodesNotVisited}
+
+    #Create prev dictioary with np.nan as values
+    prev = {node:np.nan for node in nodesNotVisited}
+
+    #inizialize source
+    dist[source] = 0
+
+    #select minimum value
+    myMin = min(dist.values())
+
+    #while it has still nodes not visited
+    while nodesNotVisited:
+        
+        #take all the nodes with min values
+        nodes = [x for x, y in dist.items() if y == myMin and x in nodesNotVisited]
+
+        #iterate over the previosy obtained nodes
+        for node in nodes:
+
+            #remove the node
+            nodesNotVisited.remove(node)
+
+            #take a list of the neighbors node that has been removed from the list
+            neighbors = list(graph.neighbors(node))
+
+            #iterate over the neighbors
+            for neighbor in neighbors:
+
+                #if the neighbor is in the list
+                if neighbor in nodesNotVisited:
+                    #update the value of the distance
+                    newDist = dist[node] + 1
+
+                    #if the new value is less than the distance of it's neigbours
+                    if newDist < dist[neighbor]:
+                        #update values
+                        dist[neighbor] = newDist
+                        prev[neighbor] = node
+                #if the neigbor is the target then i finished my search
+                if neighbor == target:
+                    #write and return the path
+                    myPath = writePath(prev, source, target)
+                    return myPath
+        #increase myMin
+        myMin = myMin+1
+    
+    #write the path at the end
+    myPath = writePath(prev, source, target)
+    return myPath
+
+
+
+
+# Visualization 3 
+
+
+def define_labels(graph, path):
+    # Get the labels just for the nodes in the path 
+    labels = {}    
+    # Iterate over the graph nodes
+    for node in graph.nodes():
+        # If I pass a node in the path
+        if node in path:
+            # Set the label to the node name
+            labels[node] = node
+        else:
+            # Else leave it empty
+            labels[node] = ""
+    return labels
+
+
+def find_edges(path):
+    # Find the edges that you go though in the path, and save them in in a list
+    f3_edge = []
+    # Iterate over path's nodes
+    for x in range(len(path)):
+        if x < len(path)-1:
+            # Append the touples that rappresent the wanted edge
+            f3_edge.append((path[x], path[x+1]))
+            f3_edge.append((path[x+1], path[x]))
+    return f3_edge
+
+
+
+
+# Functionality 4
+
+
 def bfs_unweighted(graph,source):
     Q = Queue()
     graph_dict = nx.to_dict_of_dicts(graph)
@@ -55,192 +268,10 @@ def bfs_weighted(graph,source):
     return bfs_edges
 
 
-def findNetworksHub(graph, typeG, degreeDist):
-    if typeG == 1:
-
-        #return a list with just the degree of the graph
-        tempDegree = dict(graph.degree())
-
-    #if graph of type 2
-    if typeG == 2:
-        #create a dictionary of the nosed in G2
-        dic = dict(G2.nodes(data="type"))
-
-        #we take only the degree for the comics
-        tempDegree = dict(graph.degree([k for k, v in dic.items() if v == "comic"]))
-
-    #select the threshold
-    threshold = np.quantile(list(tempDegree.values()), 0.95)
-    
-    #Select nodes based on threshold
-    nodes = [k for k, v in tempDegree.items() if v > threshold ]
-
-    return nodes
 
 
-def findNHeroes(data: pd.DataFrame, N: int) -> pd.DataFrame:
-    NHeroes = list(data.groupby(by="hero")
-    .count()                                            #count number of repetition
-    .sort_values(by="comic", ascending=False)           #Sort values by "comic"
-    .head(N)                                            #get top N
-    .reset_index()                                      #reset index
-    .hero)                                              #get only the names of our heroes
-    return NHeroes
+# Functionality 5
 
-
-def findNNodes(graph, typeG, edges):
-    #take the number of nodes
-    numberNodes = len(graph)
-    
-    #if graph is of type 2
-    if typeG == "2":
-        
-        #create a dictionary from the nodes of the graph
-        dic = dict(graph.nodes(data="type"))
-
-        #select all the nodes that are comics
-        numberComic = len([k for k, v in dic.items() if v == "comic"])
-        
-        #select all the nodes that are heroes
-        numberHero = len([k for k, v in dic.items() if v == "hero"])
-
-        #put them in a tuple
-        numberNodes = (numberComic, numberHero)
-    return numberNodes
-
-
-def findNCollaboration(graph, typeG):
-    #If graph is of type 1
-    if typeG == 1:
-
-        #return a list with just the degree of the graph
-        return list(graph.degree())
-
-    #if graph is of type 2
-    if typeG == 2:
-        
-        #select all the nodes that are comic and take the degree
-        dic = dict(G2.nodes(data="type"))
-        return list(graph.degree([k for k, v in dic.items() if v == "comic"]))
-
-
-def writePath(prev, source, target):
-    #create empty list
-    myList = []
-
-    #create boolean in case the path is not found
-    notFound = True
-    
-    #place the target in the list
-    myList += [target]
-
-    #if the value is a NaN
-    if prev[target] == np.nan:
-        return "Path Not Found!"
-    
-    #while True
-    while notFound:
-
-        #add the last node found in reverse
-        myList += [prev[myList[-1]]]
-
-        #if last one inserted is the source
-        if myList[-1] == source:
-
-            #then i found a path
-            notFound= False
-    
-    # reverse the list
-    myList.reverse()
-    return myList
-
-
-#TODO: gestisci il caso in cui non siano connessi
-def findShortestPath(graph, couple):
-    #select the source and the target
-    source = couple[0]
-    target = couple[1]
-
-    #create a set of nodes to see which one we will have already visited
-    nodesNotVisited = set(graph.nodes())
-    
-    #Create dist dictionary with np.inf as values
-    dist = {node:np.inf for node in nodesNotVisited}
-
-    #Create prev dictioary with np.nan as values
-    prev = {node:np.nan for node in nodesNotVisited}
-
-    #inizialize source
-    dist[source] = 0
-
-    #select minimum value
-    myMin = min(dist.values())
-
-    #while it has still nodes not visited
-    while nodesNotVisited:
-        
-        #take all the nodes with min values
-        nodes = [x for x, y in dist.items() if y == myMin and x in nodesNotVisited]
-
-        #iterate over the previosy obtained nodes
-        for node in nodes:
-
-            #remove the node
-            nodesNotVisited.remove(node)
-
-            #take a list of the neighbors node that has been removed from the list
-            neighbors = list(G2.neighbors(node))
-
-            #iterate over the neighbors
-            for neighbor in neighbors:
-
-                #if the neighbor is in the list
-                if neighbor in nodesNotVisited:
-                    #update the value of the distance
-                    newDist = dist[node] + 1
-
-                    #if the new value is less than the distance of it's neigbours
-                    if newDist < dist[neighbor]:
-                        #update values
-                        dist[neighbor] = newDist
-                        prev[neighbor] = node
-                #if the neigbor is the target then i finished my search
-                if neighbor == target:
-                    #write and return the path
-                    myPath = writePath(prev, source, target)
-                    return myPath
-        #increase myMin
-        myMin = myMin+1
-    
-    #write the path at the end
-    myPath = writePath(prev, source, target)
-    return myPath
-
-
-def define_labels(graph, path):
-    # Get the labels just for the nodes in the path 
-    labels = {}    
-    # Iterate over the graph nodes
-    for node in graph.nodes():
-        # If I pass a node in the path
-        if node in path:
-            # Set the label to the node name
-            labels[node] = node
-        else:
-            # Else leave it empty
-            labels[node] = ""
-    return labels
-
-def find_edges(path):
-    # Find the edges that you go though in the path, and save them in in a list
-    f3_edge = []
-    # Iterate over path's nodes
-    for x in range(len(path)):
-        if x < len(path)-1:
-            # Append the touples that rappresent the wanted edge
-            f3_edge.append((path[x], path[x+1]))
-            f3_edge.append((path[x+1], path[x]))
-    return f3_edge
 
 def edge_to_remove(graph):
 
@@ -252,6 +283,7 @@ def edge_to_remove(graph):
 
   return edge
 
+
 def edge_to_remove2(graph):
 
   #G_dict = nx.get_edge_attributes(graph, "weight")
@@ -261,6 +293,7 @@ def edge_to_remove2(graph):
   edges = sorted(G_dict.items(), key=lambda item: item[1], reverse = True)
   edge, maxVal = sorted(G_dict.items(), key=lambda item: item[1], reverse = True)[0]
   return edges, maxVal
+
 
 def girvan_newman(graph, n):
         # find the connected components

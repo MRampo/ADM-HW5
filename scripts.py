7edc94128c5be5f59164dd30fb0b963ac65f9c0f
Matteo Rampolla <matteo.rampolla96@gmail.com>
2023-01-07 12:17:09 +0100

    finished commenting + graph fix

--- scripts.py
+++ scripts.py
@@ -2,179 +2,295 @@ import networkx as nx
 import pandas as pd
 import re
 import numpy as np
+from queue import Queue 
+
+def bfs_unweighted(graph,source):
+    Q = Queue()
+    graph_dict = nx.to_dict_of_dicts(graph)
+    visited = {key: False for key in graph_dict.keys()}
+
+    Q.put((source,source))
+    visited[source] = True
+    bfs_edges = list()
+    try:
+        while Q:
+            s,path = Q.get(0)
 
 
-class Graph:
-    def __init__(self, df_hero_net=None, df_edges=None, df_nodes=None):
-        try:
-            self.df_hero_net = df_hero_net
-        except:
-            pass
-        try:
-            self.df_nodes = df_nodes
-        except:
-            pass
+            if s != source:
+                bfs_edges.append((path,s))
 
-        try:
-            self.df_edges = df_edges
-        except:
-            pass
-        self.G_2 = nx.Graph()
-        self.G_1 = nx.Graph()
+            for neighbor in graph_dict[s].keys():
+                if visited[neighbor] == False:
+                    Q.put((neighbor,s))
+                    visited[neighbor] = True
 
-    def create_graph_hero_network(self):
-        self.df_hero_net = self.df_hero_net.sort_values(by=['hero1', 'hero2']).reset_index(drop=True)
-        self.df_hero_net['Number'] = self.df_hero_net.groupby(['hero1', 'hero2']).cumcount().add(1)
-        self.df_hero_net = self.df_hero_net.sort_values(by='Number', ascending=False)
-        self.df_hero_net = self.df_hero_net.drop_duplicates(subset=['hero1', 'hero2'], keep='first')
-
-        indexes = self.df_hero_net.query("hero1 == hero2").index
-        self.df_hero_net = self.df_hero_net.drop(indexes)
-
-        mask = self.df_hero_net['hero1'] > self.df_hero_net['hero2']
-        # swap the values in those rows
-        self.df_hero_net.loc[mask, ['hero1', 'hero2']] = self.df_hero_net.loc[mask, ['hero2', 'hero1']].values
-
-        self.df_hero_net = self.df_hero_net.groupby(['hero1', 'hero2'], sort=False).agg(
-            {'hero1': 'first', 'hero2': 'first', 'Number': 'sum'}).reset_index(drop=True)
-
-        self.df_hero_net['Number'] = self.df_hero_net['Number'].apply(lambda x: round(1 / x, 3))
-
-        array_hero_net = self.df_hero_net.to_numpy()
-
-        # Now we can create the graph
-        self.G_2.add_weighted_edges_from(array_hero_net)
-
-        return self.G_2
-
-    def create_graph_edges_nodes(self):
-
-        self.G_2.add_nodes_from(self.df_nodes.loc[self.df_nodes.type == 'hero'].node, type='H')
-        self.G_2.add_nodes_from(self.df_nodes.loc[self.df_nodes.type == 'comic'].node, type='C')
-        self.G_2.add_edges_from(self.df_edges.to_numpy())
-        return self.G_2
-
-    def number_nodes(self):
-        return self.G_2.number_of_edges()
-
-    def get_Dataframe(self):
-        df_between = pd.DataFrame.from_dict(nx.degree_centrality(self.G),
-                                            orient='index',
-                                            columns=['betweenness']).sort_values(by='betweenness', ascending=False)
-        return df_between
-
-        # node_df = pd.DataFrame({attr: G.vs[attr] for attr in G.vertex_attributes()})
-        # edge_df = pd.DataFrame({attr: G.es[attr] for attr in G.edge_attributes()})
+    except:
+        pass
+    return bfs_edges
 
 
-class Preprocessing:
+def bfs_weighted(graph,source):
 
-    def __init__(self):
-        self.df_wrong = pd.read_csv('hero-network.csv')  # wrong dataset
-        self.df_right = pd.read_csv('edges.csv')  # right dataset
-        self.df_nodes = pd.read_csv('nodes.csv')
-        self.dic = {}
+    Q = Queue()
+    graph_dict = nx.to_dict_of_dicts(graph)
+    visited = {key: False for key in graph_dict.keys()}
 
-    def get_names(self):
-        # Extract all the names from the edges
-        vec_right = self.df_right.hero.unique()
-        clean_right = set(map(Preprocessing.cleaning, vec_right))
+    Q.put((source,source,0))
+    visited[source] = True
+    bfs_edges = list()
 
-        # Extract all the names of the heroes that might be wrong (col hero1)
-        vec_wrong_hero_1 = self.df_wrong.hero1.unique()
-        new_vec_wrong_hero_1 = set(map(Preprocessing.cleaning, vec_wrong_hero_1))
+    while Q.empty()==False:
+            s,path,weight = Q.get(0)
 
-        # Extract all the names of the heroes that might be wrong (col hero2)
-        vec_wrong_hero_2 = self.df_wrong.hero2.unique()
-        new_vec_wrong_hero_2 = set(map(Preprocessing.cleaning, vec_wrong_hero_2))
+            if isinstance(weight,dict):
+                bfs_edges.append((path,s,weight['weight']))
 
-        # Merge
-        merge_hero_wrong = new_vec_wrong_hero_1.union(new_vec_wrong_hero_2)
+            for neighbor,weight in graph_dict[s].items():
+                if visited[neighbor] == False:
+                    Q.put((neighbor,s,weight))
+                    visited[neighbor] = True
 
-        self.dic = {}
-        # Iterate over the wrong hero names
-        for x in merge_hero_wrong:
 
-            # Iterate over the right hero names
-            for y in clean_right:
-                self.dic[x] = x
-                if x in y:
-                    self.dic[x] = y
+    return bfs_edges
+
+
+def findNetworksHub(graph, typeG, degreeDist):
+    if typeG == 1:
+
+        #return a list with just the degree of the graph
+        tempDegree = dict(graph.degree())
+
+    #if graph of type 2
+    if typeG == 2:
+        #create a dictionary of the nosed in G2
+        dic = dict(G2.nodes(data="type"))
+
+        #we take only the degree for the comics
+        tempDegree = dict(graph.degree([k for k, v in dic.items() if v == "comic"]))
+
+    #select the threshold
+    threshold = np.quantile(list(tempDegree.values()), 0.95)
+    
+    #Select nodes based on threshold
+    nodes = [k for k, v in tempDegree.items() if v > threshold ]
+
+    return nodes
+
+
+def findNHeroes(data: pd.DataFrame, N: int) -> pd.DataFrame:
+    NHeroes = list(data.groupby(by="hero")
+    .count()                                            #count number of repetition
+    .sort_values(by="comic", ascending=False)           #Sort values by "comic"
+    .head(N)                                            #get top N
+    .reset_index()                                      #reset index
+    .hero)                                              #get only the names of our heroes
+    return NHeroes
+
+
+def findNNodes(graph, typeG, edges):
+    #take the number of nodes
+    numberNodes = len(graph)
+    
+    #if graph is of type 2
+    if typeG == "2":
+        
+        #create a dictionary from the nodes of the graph
+        dic = dict(graph.nodes(data="type"))
+
+        #select all the nodes that are comics
+        numberComic = len([k for k, v in dic.items() if v == "comic"])
+        
+        #select all the nodes that are heroes
+        numberHero = len([k for k, v in dic.items() if v == "hero"])
+
+        #put them in a tuple
+        numberNodes = (numberComic, numberHero)
+    return numberNodes
+
+
+def findNCollaboration(graph, typeG):
+    #If graph is of type 1
+    if typeG == 1:
+
+        #return a list with just the degree of the graph
+        return list(graph.degree())
+
+    #if graph is of type 2
+    if typeG == 2:
+        
+        #select all the nodes that are comic and take the degree
+        dic = dict(G2.nodes(data="type"))
+        return list(graph.degree([k for k, v in dic.items() if v == "comic"]))
+
+
+def writePath(prev, source, target):
+    #create empty list
+    myList = []
+
+    #create boolean in case the path is not found
+    notFound = True
+    
+    #place the target in the list
+    myList += [target]
+
+    #if the value is a NaN
+    if prev[target] == np.nan:
+        return "Path Not Found!"
+    
+    #while True
+    while notFound:
+
+        #add the last node found in reverse
+        myList += [prev[myList[-1]]]
+
+        #if last one inserted is the source
+        if myList[-1] == source:
+
+            #then i found a path
+            notFound= False
+    
+    # reverse the list
+    myList.reverse()
+    return myList
+
+
+#TODO: gestisci il caso in cui non siano connessi
+def findShortestPath(graph, couple):
+    #select the source and the target
+    source = couple[0]
+    target = couple[1]
+
+    #create a set of nodes to see which one we will have already visited
+    nodesNotVisited = set(graph.nodes())
+    
+    #Create dist dictionary with np.inf as values
+    dist = {node:np.inf for node in nodesNotVisited}
+
+    #Create prev dictioary with np.nan as values
+    prev = {node:np.nan for node in nodesNotVisited}
+
+    #inizialize source
+    dist[source] = 0
+
+    #select minimum value
+    myMin = min(dist.values())
+
+    #while it has still nodes not visited
+    while nodesNotVisited:
+        
+        #take all the nodes with min values
+        nodes = [x for x, y in dist.items() if y == myMin and x in nodesNotVisited]
+
+        #iterate over the previosy obtained nodes
+        for node in nodes:
+
+            #remove the node
+            nodesNotVisited.remove(node)
+
+            #take a list of the neighbors node that has been removed from the list
+            neighbors = list(G2.neighbors(node))
+
+            #iterate over the neighbors
+            for neighbor in neighbors:
+
+                #if the neighbor is in the list
+                if neighbor in nodesNotVisited:
+                    #update the value of the distance
+                    newDist = dist[node] + 1
+
+                    #if the new value is less than the distance of it's neigbours
+                    if newDist < dist[neighbor]:
+                        #update values
+                        dist[neighbor] = newDist
+                        prev[neighbor] = node
+                #if the neigbor is the target then i finished my search
+                if neighbor == target:
+                    #write and return the path
+                    myPath = writePath(prev, source, target)
+                    return myPath
+        #increase myMin
+        myMin = myMin+1
+    
+    #write the path at the end
+    myPath = writePath(prev, source, target)
+    return myPath
+
+
+def define_labels(graph, path):
+    # Get the labels just for the nodes in the path 
+    labels = {}    
+    # Iterate over the graph nodes
+    for node in graph.nodes():
+        # If I pass a node in the path
+        if node in path:
+            # Set the label to the node name
+            labels[node] = node
+        else:
+            # Else leave it empty
+            labels[node] = ""
+    return labels
+
+def find_edges(path):
+    # Find the edges that you go though in the path, and save them in in a list
+    f3_edge = []
+    # Iterate over path's nodes
+    for x in range(len(path)):
+        if x < len(path)-1:
+            # Append the touples that rappresent the wanted edge
+            f3_edge.append((path[x], path[x+1]))
+            f3_edge.append((path[x+1], path[x]))
+    return f3_edge
+
+def edge_to_remove(graph):
+
+  #G_dict = nx.get_edge_attributes(graph, "weight")
+  G_dict = nx.edge_betweenness_centrality(graph,weight ="weight", normalized=True)
+
+  # extract the edge with highest edge betweenness centrality score
+  edge, value = sorted(G_dict.items(), key=lambda item: item[1], reverse = True)[0]
+
+  return edge
+
+def edge_to_remove2(graph):
+
+  #G_dict = nx.get_edge_attributes(graph, "weight")
+  G_dict = nx.edge_betweenness_centrality(graph,weight ="weight", normalized=True)
+
+  # extract the edge with highest edge betweenness centrality score
+  edges = sorted(G_dict.items(), key=lambda item: item[1], reverse = True)
+  edge, maxVal = sorted(G_dict.items(), key=lambda item: item[1], reverse = True)[0]
+  return edges, maxVal
+
+def girvan_newman(graph, n):
+        # find the connected components
+        sg = nx.connected_components(graph)
+
+        # find the number of connected components
+        sg_count = nx.number_connected_components(graph)
+
+        #counter for the minimum number of edged needed to be cut to create a community
+        count_edges = 0
+
+        #while we have less than n community, we will keep on removing the edges
+        while(sg_count < n):
+            #select the edges to remove
+            edges, maxVal = edge_to_remove2(graph)
+            edges = [edge for edge, val in edges if val == maxVal]
+
+            for edge in edges:
+                #increase the counter
+                count_edges += 1
+                #remove the edge
+                graph.remove_edge(edge[0], edge[1])
+
+                #update the connected components
+                sg = nx.connected_components(graph)
+
+                #Uptdate the number of connected components
+                sg_count = nx.number_connected_components(graph)
+
+                if sg_count == n:
                     break
-        return self.dic
-
-    def cleaning_dataset(self):
-        # Clean the values in both columns
-        self.df_wrong.hero1 = self.df_wrong.hero1.apply(lambda row: Preprocessing.cleaning(row))
-        self.df_wrong.hero2 = self.df_wrong.hero2.apply(lambda row: Preprocessing.cleaning(row))
-
-        # Clean edges.csv
-
-        self.df_right.hero = self.df_right.hero.apply(lambda row : Preprocessing.cleaning(row))
-
-        # Replace the wrong names with the right ones
-        self.dic = Preprocessing().get_names()
-        self.df_wrong["hero1"] = self.df_wrong.hero1.apply(lambda row: self.dic[row])
-        self.df_wrong["hero2"] = self.df_wrong.hero2.apply(lambda row: self.dic[row])
-
-        # Clean the values in nodes.node
-        self.df_nodes.node = self.df_nodes.node.apply(lambda row: Preprocessing.cleaning(row))
-        self.df_nodes.node.loc[self.df_nodes.node == 'SPIDER-MAN/PETER PARKERKER'] = 'SPIDER-MAN/PETER PARKER'
-
-        # remove duplicates rows from hero network
-        self.df_wrong = self.df_wrong[self.df_wrong.hero1 != self.df_wrong.hero2]
-
-        return self.df_wrong, self.df_right, self.df_nodes
-
-    @staticmethod
-    def cleaning(string):
-        # Pattern for backslash in last position
-        pattern = "\/$"
-        new_string = re.sub(pattern, "", string)
-        # pattern for space in last position
-        pattern = " $"
-        new_string = re.sub(pattern, "", new_string)
-
-        return new_string
-
-    def findNHeroes(edges: pd.DataFrame, N: int) -> pd.DataFrame:
-        NHeroes = list(df_edges.groupby(by="hero")
-                       .count()  # count number of repetition
-                       .sort_values(by="comic", ascending=False)  # Sort values by "comic"
-                       .head(N)  # get top N
-                       .reset_index()  # reset index
-                       .hero)  # get only the names of our heroes
-        return NHeroes
-
-    def functionality_2(graph, metric, node, n_heroes=None):
-        if n_heroes != None:
-            n = findNHeroes(df_edges, n_heroes)
-
-        if metric == 'Betweeness':
-            betweenness = nx.betweenness_centrality(graph, k=10, seed=1234, weight='weight')
-
-            output = pd.DataFrame.from_dict(betweenness, orient='index',
-                                            columns=['Betweenness']).sort_values(by='Betweenness', ascending=False)
-            try:
-                whole_output = output.loc[n, :]
-            except:
-                pass
-        if metric == 'Closeness':
-            closeness = nx.closeness_centrality(graph, distance='weight')
-            output = pd.DataFrame.from_dict(closeness, orient='index',
-                                            columns=['Closeness']).sort_values(by='Closeness', ascending=False)
-            try:
-                whole_output = output.loc[n, :]
-            except:
-                pass
-
-        if metric == 'PageRank':
-            pagerank = nx.pagerank(graph, weight='weight')
-            output = pd.DataFrame.from_dict(pagerank, orient='index',
-                                            columns=['PageRank']).sort_values(by='PageRank', ascending=False)
-            try:
-                whole_output = output.loc[n, :]
-            except:
-                pass
-
-        return output.loc[node, :], whole_output
+        
+        return sg, count_edges, graph
